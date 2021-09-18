0d7d5097d267da73ececbfe912df0907344dbe1f
Jonathan Kovach <jlkovach01@gmail.com>
2021-09-17 22:21:59 -0400

    added app config

--- src/services/NewLake.Api/Program.cs
+++ src/services/NewLake.Api/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+
 namespace NewLake.Api
 {
     public class Program
@@ -13,8 +16,25 @@ namespace NewLake.Api
             Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
+                webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(settings["ConnectionStrings:AppConfig"])
+                                .ConfigureKeyVault(kv =>
+                                {
+                                    var tenantId = settings["AppSettings:TenantId"];
+                                    var clientId = settings["AppSettings:ClientId"];
+                                    var clientSecret = settings["AppSettings:ClientSecret"];
+
+                                    kv.SetCredential(
+                                        new ClientSecretCredential(tenantId, clientId, clientSecret));
+                                });
+                    });
+                });
                 webBuilder.UseStartup<Startup>();
             });
-    
     }
 }
