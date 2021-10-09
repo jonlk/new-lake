af6e89afeb93d259768fb63a5f21ce170475de39
Jonathan Kovach <jlkovach01@gmail.com>
2021-10-09 02:23:05 -0400

    quick cache subscriber

--- src/services/NewLake.Api/Infrastructure/Extensions/DependencyRegistrationExtensions.cs
+++ src/services/NewLake.Api/Infrastructure/Extensions/DependencyRegistrationExtensions.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NewLake.Core;
 using NewLake.Core.Services.Messaging;
+using StackExchange.Redis;
 using StackExchange.Redis.Extensions.Core.Configuration;
 using StackExchange.Redis.Extensions.System.Text.Json;
 
@@ -11,17 +13,21 @@ namespace NewLake.Api.Infrastructure.Extensions
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //rabbit mq
-            services.AddSingleton(typeof(IMessageService<>), typeof(MessageService<>));
-
-            //redis cache
             services
                 .AddStackExchangeRedisExtensions<SystemTextJsonSerializer>((options) =>
             {
                 return configuration.GetSection("Redis").Get<RedisConfiguration>();
             });
 
+            var muxer = ConnectionMultiplexer.Connect("localhost,allowAdmin=true");
+
+            muxer.GetServer(muxer.GetEndPoints().Single())
+                .ConfigSet("notify-keyspace-events", "K$");
+
+            services.AddSingleton<IConnectionMultiplexer>(muxer);
+            services.AddSingleton(typeof(IMessageService<>), typeof(MessageService<>));
             services.AddSingleton<ICacheService, CacheService>();
+
             return services;
         }
     }
