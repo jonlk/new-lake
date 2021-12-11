44d3e1b854fa55926c0ec52ed25614cd3df005df
jonlk <jlkovach01@gmail.com>
2021-12-10 19:22:44 -0500

    added grpc

---
+++ src/services/NewLake.Core/Services/Bulk/BulkInfoService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using NewLake.Core.GrpcProto;
+
+namespace NewLake.Core.Services.Bulk
+{
+    public class BulkInfoService
+        : NewLakeReceiverService.NewLakeReceiverServiceBase
+    {
+
+        private readonly ILogger<BulkInfoService> _logger;
+
+        public BulkInfoService(ILogger<BulkInfoService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ReturnMessage> SendBulkMessage(InfoMessage request,
+                                                            ServerCallContext context)
+        {
+            var returnMessage = new ReturnMessage
+            {
+                InfoMessage = "Info about the event"
+            };
+
+            return Task.FromResult(returnMessage);
+        }
+    }
+}
---
+++ src/services/NewLake.GrpcReceive.Service/Services/BulkInfoServiceClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLake.GrpcReceive.Service.Services
+{
+    internal class BulkInfoServiceClient
+    {
+
+    }
+}
