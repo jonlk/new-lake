20ac4c34c0ea78177e58823f36d4764d91d7365f
jonlk <jlkovach01@gmail.com>
2021-12-17 13:21:21 -0500

    removed azure config

--- src/services/NewLake.Api/Program.cs
+++ src/services/NewLake.Api/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,36 +18,8 @@ namespace NewLake.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureLogging(logging =>
-            {
-                logging.AddFilter((provider, category, logLevel) =>
-                {
-                    if (category.Contains("NewLake"))
-                    {
-                        return true;
-                    }
-                    return false;
-                });
-                logging.AddConsole();
-            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var settings = config.Build();
-                    config.AddAzureAppConfiguration(options =>
-                    {
-                        options.Connect(settings["ConnectionStrings:AppConfig"])
-                                .ConfigureKeyVault(kv =>
-                                {
-                                    kv.SetCredential(
-                                        new ClientSecretCredential(
-                                            settings["AppSettings:TenantId"],
-                                            settings["AppSettings:ClientId"],
-                                            settings["AppSettings:ClientSecret"]));
-                                });
-                    });
-                });
                 webBuilder.UseStartup<Startup>();
             });
     }
